a6b628402af8e6b35eb80c1b7b46cfc9cb900f94
sany <eo15423@gmail.com>
2020-05-26 09:30:14 +0900

    db연결중..

--- src/testBoard/TestDAO.java
+++ src/testBoard/TestDAO.java
@@ -1,5 +1,28 @@
 package testBoard;
 
-public class TestDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class TestDAO {
+	
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String user = "jsp";
+	private String pwd = "1234";
+	private Connection con;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	public TestDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
 }
--- src/testBoard/TestDTO.java
+++ src/testBoard/TestDTO.java
@@ -2,4 +2,37 @@ package testBoard;
 
 public class TestDTO {
 
+	
+	 private int num;
+	 private String pdate;
+	 private String title;
+	 private int count;
+	 
+	 
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getPdate() {
+		return pdate;
+	}
+	public void setPdate(String pdate) {
+		this.pdate = pdate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	 
+	 
 }
